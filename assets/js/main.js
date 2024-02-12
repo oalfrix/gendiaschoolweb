ab23d669f51f8495847e921f40ae53f6ca234e23
alfrixo <140119318+oalfrix@users.noreply.github.com>
2024-02-12 16:06:26 +0300

    Add files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,29 @@
+/*=============== SHOW MENU ===============*/
+const showMenu = (toggleId, navId) =>{
+   const toggle = document.getElementById(toggleId),
+         nav = document.getElementById(navId)
+
+   toggle.addEventListener('click', () =>{
+       // Add show-menu class to nav menu
+       nav.classList.toggle('show-menu')
+
+       // Add show-icon to show and hide the menu icon
+       toggle.classList.toggle('show-icon')
+   })
+}
+
+showMenu('nav-toggle','nav-menu')
+
+
+//------------Gallery Script----------
+
+  document.querySelectorAll('.image-container img').forEach(image => {
+    image.onclick = () => {
+      document.querySelector('.popup-image').style.display = 'block';
+      document.querySelector('.popup-image img').src = image.getAttribute('src');
+    }
+  });
+
+  document.querySelector('.popup-image span').onclick = () =>{
+    document.querySelector('.popup-image').style.display = 'none';
+  }
