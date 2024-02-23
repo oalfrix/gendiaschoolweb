1af1ce20edccefffc449ff6d4fb272c4f658275a
alfrixo <140119318+oalfrix@users.noreply.github.com>
2024-02-23 17:50:27 +0300

    update3

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,19 +1,27 @@
 /*=============== SHOW MENU ===============*/
-const showMenu = (toggleId, navId) =>{
-   const toggle = document.getElementById(toggleId),
-         nav = document.getElementById(navId)
+const showMenu = (toggleId, navId) => {
+  const toggle = document.getElementById(toggleId),
+        nav = document.getElementById(navId);
 
-   toggle.addEventListener('click', () =>{
-       // Add show-menu class to nav menu
-       nav.classList.toggle('show-menu')
+  toggle.addEventListener('click', () => {
+      // Add show-menu class to nav menu
+      nav.classList.toggle('show-menu');
 
-       // Add show-icon to show and hide the menu icon
-       toggle.classList.toggle('show-icon')
-   })
+      // Add show-icon to show and hide the menu icon
+      toggle.classList.toggle('show-icon');
+  });
+
+  // Close sidebar when clicking outside of it
+  document.addEventListener('click', (event) => {
+      if (!nav.contains(event.target) && !toggle.contains(event.target)) {
+          nav.classList.remove('show-menu');
+          toggle.classList.remove('show-icon');
+      }
+  });
 }
 
-showMenu('nav-toggle','nav-menu')
 
+showMenu('nav-toggle','nav-menu')
 
 //------------Gallery Script----------
 
